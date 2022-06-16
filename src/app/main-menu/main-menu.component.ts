44baa48f00810e2dbf786d726ba0164e628993b2
Mike Mandrea <mike@mandrea.net>
2022-06-15 19:15:05 -0500

    if cnc change title and hide filament menu

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 
 import { AppService } from '../app.service';
 
+import { ConfigService } from '../config/config.service';
+
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss'],
 })
 export class MainMenuComponent {
-  public constructor(public service: AppService) {}
+  public constructor(public service: AppService,
+                     private configService: ConfigService,) {
+                       this.cncMode = configService.iscncMode()
+                     }
 
   public settings = false;
+  public cncMode = false;
 
   public showSettings(): void {
     this.settings = true;
